76f11d6ae0884c819d75ed11affbf66a0511cbb5
Zachary Diehl <zachary.diehl@gmail.com>
2020-07-21 08:01:50 -0400

    team details displaying user details for the currently selected team, also styling updates based on selected team

--- src/mainMenu/Teams.jsx
+++ src/mainMenu/Teams.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from '@reach/router'
+import { Link, navigate } from '@reach/router'
 
 import { addTeam, getAllTeams, getUser } from '../db/firebase'
+
+import './Teams.css'
 
 const Teams = ({ currentUser }) => {
 	const [teamsState, setTeamsState] = useState([])
 	const [selectedTeamState, setSelectedTeamState] = useState('')
+	const [userDetailsState, setUserDetailsState] = useState()
 	const [newTeamNameState, setNewTeamNameState] = useState('')
 	const [newTeamMemberState, setNewTeamMemberState] = useState('')
 	const [allTeamMembersState, setAllTeamMembersState] = useState([])
@@ -16,6 +19,7 @@ const Teams = ({ currentUser }) => {
 			console.log(user)
 			const teams = await getAllTeams(user)
 			setTeamsState(teams)
+			console.log('teams', teams)
 		}
 		fetchTeams()
 	}, [currentUser.email])
@@ -36,44 +40,74 @@ const Teams = ({ currentUser }) => {
 		teamAdded && setTeamsState(teamsState.concat(newTeam))
 	}
 
-	console.log(teamsState)
+	const handleSelectTeam = event => {
+		const { id } = event.target
+		const { uid } = currentUser
+		setSelectedTeamState(event.target.id)
+		const selectedTeam = teamsState.find(team => team.name === id)
+		const userDetails = selectedTeam.members[uid]
+		setUserDetailsState(userDetails)
+	}
 
 	return (
-		<div>
-			<ul>
-				{teamsState.length ? (
-					teamsState.map(team => {
-						return (
-							<li>
-								<Link to={`/dashboard/${team.name}`} key={team.name}>
+		<div className='teams__landing'>
+			<div className='teams__details'>
+				<h1>{selectedTeamState}</h1>
+				<div>
+					{userDetailsState && (
+						<ul>
+							<li>Name: {userDetailsState.userName}</li>
+							<li>Experience: {userDetailsState.experience}</li>
+							<li>Currency: {userDetailsState.currency}</li>
+							<li>Level: {userDetailsState.level}</li>
+						</ul>
+					)}
+				</div>
+			</div>
+			<div className='teams__list'>
+				<ul>
+					{teamsState.length ? (
+						teamsState.map(team => {
+							return (
+								<li
+									id={team.name}
+									onClick={handleSelectTeam}
+									className={`app__list__item ${team.name ===
+										selectedTeamState && 'selected'}`}
+								>
 									{team.name}
-								</Link>
-							</li>
-						)
-					})
-				) : (
-					<p>No Teams</p>
+								</li>
+							)
+						})
+					) : (
+						<p>No Teams</p>
+					)}
+				</ul>
+				{selectedTeamState && (
+					<button onClick={() => navigate(`/dashboard/${selectedTeamState}`)}>
+						Enter World
+					</button>
 				)}
-			</ul>
-			<form onSubmit={handleSubmit}>
-				<input
-					placeholder='Team Name'
-					onChange={event => setNewTeamNameState(event.target.value)}
-				></input>
-				<input
-					placeholder='Add Member'
-					onChange={event => setNewTeamMemberState(event.target.value)}
-				></input>
-				<button onClick={handleNewTeamMember}>Add Member</button>
-				{allTeamMembersState.length && (
-					<ul>
-						{allTeamMembersState.map(member => (
-							<li key={member}>{member}</li>
-						))}
-					</ul>
-				)}
-				<button type='submit'>Add Team</button>
-			</form>
+				<form onSubmit={handleSubmit}>
+					<input
+						placeholder='Team Name'
+						onChange={event => setNewTeamNameState(event.target.value)}
+					></input>
+					<input
+						placeholder='Add Member'
+						onChange={event => setNewTeamMemberState(event.target.value)}
+					></input>
+					<button onClick={handleNewTeamMember}>Add Member</button>
+					{allTeamMembersState.length && (
+						<ul>
+							{allTeamMembersState.map(member => (
+								<li key={member}>{member}</li>
+							))}
+						</ul>
+					)}
+					<button type='submit'>Add Team</button>
+				</form>
+			</div>
 		</div>
 	)
 }
